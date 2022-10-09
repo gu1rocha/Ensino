36b312eb7dbf2c308aa4c663e8544e359cd89f42
Guilherme Rocha <guirocha12395@gmail.com>
2022-10-09 11:59:44 -0300

    Adicionando verificação e finalização

--- Ensino/Matematica/PrimeiroAno/Adicao/assets/js/script.js
+++ Ensino/Matematica/PrimeiroAno/Adicao/assets/js/script.js
@@ -1,72 +1,75 @@
-const box = document.querySelector('.game')
+const boxGame = document.querySelector('.boxGame')
+const boxMacas = boxGame.querySelector('.boxMacas')
+const boxLaranjas = boxGame.querySelector('.boxLaranjas')
+const boxResult = boxGame.querySelector('.boxResult')
 const boxConf = document.querySelector('.boxConf')
 const btnJogar = boxConf.querySelector('.jogar')
 const boxRealod = document.querySelector('.boxRealod')
-const boxGame = document.querySelector('.boxGame')
 
-let classes = []
-let quadrados = [12,13,21,22,23,24,32,33]
-let circulos =  [13,22,24,31,32,34,35,42,44,53]
+const certo = document.querySelector('#certo')
+const errado = document.querySelector('#errado')
 
 let error = false, verificador = 1, text = '', numMaca = 0, numLaranja = 0
 
-let gerarNum = () => Math.floor(Math.random() * 4) + 1;
-
-let RemoverTextoClasse = obj => obj = obj.className.replace(/[^0-9]/g,'')
+let gerarNum = (max) => Math.floor(Math.random() * max) + 1;
 
 let GerarFrutas = (local, srcImg, qtd)=>{
     local.innerHTML = ''
-
     for (let i = 0; i < qtd; i++) {
         let fruta = `<img src="${srcImg}">`
         local.innerHTML += fruta
     }
-    
 }
 
-
+let PlayAudio = elemento =>{
+    elemento.pause()
+    elemento.currentTime = 0;
+    elemento.play()
+  }
 
 let endGame = obj =>{
-    boxConf.querySelector('h1').textContent = obj.text
-    boxConf.querySelector('h1').style.color = obj.color
+    boxConf.querySelector('h1').textContent = obj.h1Text
+    boxConf.querySelector('h1').style.color = obj.h1Color
+    boxConf.querySelector('h2').textContent = obj.h2Text
+    boxConf.querySelector('.jogar').textContent = obj.btnText
     boxConf.removeAttribute('style')
     boxRealod.style.display = 'none';
 }
 
-let Verificar = (inputVerificar)=>{
-    for (const input of box.querySelectorAll('input')) {
-        if(inputVerificar.value < 1){
-            error = true
-            inputVerificar.classList.add('error')
-        }
-        if(RemoverTextoClasse(input) !== RemoverTextoClasse(inputVerificar)){
-            if(Math.abs(+RemoverTextoClasse(inputVerificar)[0] - +RemoverTextoClasse(input)[0]) <= 1){
-                if(Math.abs(+RemoverTextoClasse(inputVerificar)[1] - +RemoverTextoClasse(input)[1]) <= verificador){
-                    if(Math.abs(inputVerificar.value - input.value) <= 1){
-                        error = true
-                        inputVerificar.classList.add('error')
-                        input.classList.add('error')
-                    }
-                }
-            }
-        }
+let ValidarDados = (inputVerificar, valor, error)=>{
+    if(+inputVerificar.value !== valor){
+        inputVerificar.classList.add('error')
+        return true
     }
+    inputVerificar.classList.remove('error')
+    return error
 }
 
 
 document.querySelector('button').addEventListener(('click'),()=>{
     error = false
-    for (const input of box.querySelectorAll('input')) {
-        input.classList.remove('error')
-        Verificar(input)
-    }
+    error = ValidarDados(boxMacas.querySelector('input'), numMaca, error)
+    error = ValidarDados(boxLaranjas.querySelector('input'), numLaranja, error)
+    error = ValidarDados(boxResult.querySelector('input'), (numMaca + numLaranja), error)
 
-    error ? boxRealod.removeAttribute('style') : endGame({text: 'PARABÉNS!!!', color: 'green'})
+    if(error){
+        PlayAudio(errado)
+        boxRealod.removeAttribute('style')
+    }else{
+        PlayAudio(certo)
+        endGame({h1Text: 'PARABÉNS!!!', h2Text: 'VAMOS CONTINUAR?', h1Color: 'green', btnText: 'CONTINUAR'})
+    }
 })
 
 btnJogar.addEventListener('click',()=>{
+
+    boxMacas.querySelector('input').value = ''
+    boxLaranjas.querySelector('input').value = ''
+    boxResult.querySelector('input').value = ''
+
     boxRealod.style.display = 'none';
     boxConf.style.display = 'none';
-    GerarFrutas(document.querySelector('.boxMacas'),'assets/img/maca.png',gerarNum())
-    GerarFrutas(document.querySelector('.boxLaranjas'),'assets/img/laranja.png',gerarNum())
+    numMaca = gerarNum(4); numLaranja = gerarNum(4)
+    GerarFrutas(boxMacas.querySelector('.macas'),'assets/img/maca.png', numMaca)
+    GerarFrutas(boxLaranjas.querySelector('.laranjas'),'assets/img/laranja.png', numLaranja)
 })
